4fdc43a69bf74fecac318d2790795d3ad37df2b4
TomSadeh <dtsj89@gmail.con>
2023-07-19 16:03:03 +0300

    Update deciles.py

--- deciles.py
+++ deciles.py
@@ -49,7 +49,7 @@ data = load_data('limits', path, i='decile')
 
 st.markdown("<h1 style='text-align: center;'>?באיזה עשירון אתם</h1>", unsafe_allow_html=True)
 st.markdown("<div style='text-align: center;'>עשירון תחתון = 1, עשירון עליון = 10</div>", unsafe_allow_html=True)
-st.markdown("<div style='text-align: center;'>:הכניסו את ההכנסות נטו של משק הבית שלכם מכלל המקורות</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center;'>:הכניסו את ההכנסות החודשיות נטו של משק הבית שלכם מכלל המקורות</div>", unsafe_allow_html=True)
 income = st.number_input(":הכניסו את ההכנסות החודשיות נטו של משק הבית מכלל המקורות", 
                          min_value=1000, 
                          max_value=1000000000,

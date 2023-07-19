c49b2a8d2e96887626389abf8f6bfd72abd1a03a
TomSadeh <dtsj89@gmail.con>
2023-07-19 11:25:39 +0300

    Update deciles.py

--- deciles.py
+++ deciles.py
@@ -68,4 +68,4 @@ st.markdown("<h2 style='text-align: center;'>:משק הבית שלך בעשיר�
 st.markdown("<h1 style='text-align: center;'>{}</h1>".format(decile), unsafe_allow_html=True)
 st.markdown("<div style='text-align: center;'>לפי סקר הוצאות משק הבית 2021 של הלשכה המרכזית לסטטיסטיקה</div>", unsafe_allow_html=True)
 
-st.markdown("###### [![this is an image link](https://upload.wikimedia.org/wikipedia/commons/thumb/6/6f/Logo_of_Twitter.svg/512px-Logo_of_Twitter.svg.png)](https://twitter.com/tom_sadeh)")
+st.markdown("###### [![this is an image link](https://png.pngtree.com/element_our/sm/20180626/sm_5b32227ca3eb9.jpg)](https://twitter.com/tom_sadeh)")
